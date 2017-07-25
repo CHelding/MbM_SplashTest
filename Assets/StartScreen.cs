cbe126d9240c2d9b92693f21f6bb0cdc84b5973c
CHelding <helding13@gmail.com>
2017-07-25 14:42:10 +0200

    StartScreen

---
+++ Assets/StartScreen.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class StartScreen : MonoBehaviour {
+
+    //public Button startButton;
+
+	// Use this for initialization
+	void Start () {
+
+        //startButton = startButton.GetComponent<Button>();
+		
+	}
+
+    public void StartAR() {
+        SceneManager.LoadScene("MbM_Test");
+
+    }
+
+}
